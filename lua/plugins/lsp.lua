2c8cacd96f90becf5fcde829a2a6241ea9363c94
kotek <scpktk@gmail.com>
2024-06-12 22:27:59 +0900

    Enable lua annotation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,6 +76,52 @@ return {
             }
           })
         end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+
+          ---@param names string[]
+          ---@return string[]
+          local function get_plugin_paths(names)
+            local plugins = require("lazy.core.config").plugins
+            local paths = {}
+            for _, name in ipairs(names) do
+              if plugins[name] then
+                table.insert(paths, plugins[name].dir .. "/lua")
+              else
+                vim.notify("Invalid plugin name: " .. name)
+              end
+            end
+            return paths
+          end
+
+          ---@param plugins string[]
+          ---@return string[]
+          local function library(plugins)
+            local paths = get_plugin_paths(plugins)
+            table.insert(paths, vim.fn.stdpath("config") .. "/lua")
+            table.insert(paths, vim.env.VIMRUNTIME .. "/lua")
+            table.insert(paths, "${3rd}/luv/library")
+            table.insert(paths, "${3rd}/busted/library")
+            table.insert(paths, "${3rd}/luassert/library")
+            return paths
+          end
+
+          lspconfig.lua_ls.setup({
+            settings = {
+              Lua = {
+                runtime = {
+                  version = "LuaJIT",
+                  pathStrict = true,
+                  path = { "?.lua", "?/init.lua" },
+                },
+                workspace = {
+                  library = library({ "lazy.nvim", "telescope-frecency.nvim" }),
+                  checkThirdParty = "Disable",
+                },
+              },
+            },
+          })
+        end
       })
     end,
   },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,25 +14,32 @@ return {
   },
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set("n", "<leader><leader>", function()
+    vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>ff", function()
       require("telescope").extensions.frecency.frecency {
         workspace = "CWD"
       }
     end)
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find from Buffers" })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find Helps" })
+    vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = "Find Commands" })
     vim.keymap.set('n', '<leader>fC', function()
       builtin.colorscheme { enable_preview = true }
-    end, {})
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+    end, { desc = "Find Colorschemes" })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Find Keymaps" })
     require('telescope').setup {
       defaults = {
         file_ignore_patterns = {
           "node_modules",
           "target",
           ".git"
+        }
+      },
+      extensions = {
+        ---@type FrecencyOpts
+        frecency = {
+          matcher = "fuzzy",
         }
       }
     }
